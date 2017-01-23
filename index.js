779a890ddd76c4b3a7f76be8e60f2bf5f258d137
mstraughan86 <mstraughan86@gmail.com>
2017-01-23 14:28:53 -0800

    Test Commit III
    Signed-off-by: mstraughan86 <mstraughan86@gmail.com>

--- index.js
+++ index.js
@@ -20,6 +20,67 @@ slackTerminal.init(token, {
 	COMMAND_DIR: __dirname + '/commands'
 });
 
+
+
+///////////////// Self Updater /////////////////
+var AutoUpdater = require('auto-updater');
+
+var autoupdater = new AutoUpdater({
+ pathToJson: '',
+ autoupdate: true,
+ checkgit: false,
+ jsonhost: 'https://raw.githubusercontent.com/mstraughan86/Emeralda-AI/master/package.json',
+ contenthost: 'https://api.github.com/repos/mstraughan86/Emeralda-AI/zipball/master',
+ progressDebounce: 0,
+ devmode: false
+});
+
+// State the events
+autoupdater.on('git-clone', function() {
+  console.log("You have a clone of the repository. Use 'git pull' to be up-to-date");
+});
+autoupdater.on('check.up-to-date', function(v) {
+  console.info("You have the latest version: " + v);
+});
+autoupdater.on('check.out-dated', function(v_old, v) {
+  console.warn("Your version is outdated. " + v_old + " of " + v);
+  autoupdater.fire('download-update'); // If autoupdate: false, you'll have to do this manually.
+  // Maybe ask if the'd like to download the update.
+});
+autoupdater.on('update.downloaded', function() {
+  console.log("Update downloaded and ready for install");
+  autoupdater.fire('extract'); // If autoupdate: false, you'll have to do this manually.
+});
+autoupdater.on('update.not-installed', function() {
+  console.log("The Update was already in your folder! It's read for install");
+  autoupdater.fire('extract'); // If autoupdate: false, you'll have to do this manually.
+});
+autoupdater.on('update.extracted', function() {
+  console.log("Update extracted successfully!");
+  console.warn("RESTART THE APP!");
+});
+autoupdater.on('download.start', function(name) {
+  console.log("Starting downloading: " + name);
+});
+autoupdater.on('download.progress', function(name, perc) {
+  process.stdout.write("Downloading " + perc + "% \033[0G");
+});
+autoupdater.on('download.end', function(name) {
+  console.log("Downloaded " + name);
+});
+autoupdater.on('download.error', function(err) {
+  console.error("Error when downloading: " + err);
+});
+autoupdater.on('end', function() {
+  console.log("The app is ready to function");
+});
+autoupdater.on('error', function(name, e) {
+  console.error(name, e);
+});
+
+// Start checking
+// autoupdater.fire('check');
+
 ///////////////// GitHiub Listener Server /////////////////
 
 var http = require('http');
@@ -40,17 +101,15 @@ handler.on('error', function (err) {
 })
 
 handler.on('push', function (event) {
-	postRequest('Received a push event for ' + 
+	postRequest('IIIII - Received a push event for ' + 
     event.payload.repository.name + 
 	' to ' + 
     event.payload.ref);
   console.log('Received a push event for %s to %s',
     event.payload.repository.name,
     event.payload.ref)
+	autoupdater.fire('check');
 })
-
-///////////////// The Rest /////////////////
-console.log('hello');
 
 
 
