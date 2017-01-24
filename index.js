d15dacccc8b6f6d1e8b559bd60c1d99dad5dfc2c
mstraughan86 <mstraughan86@gmail.com>
2017-01-23 18:05:30 -0800

    Testing Auto-Updater Again
    Signed-off-by: mstraughan86 <mstraughan86@gmail.com>

--- index.js
+++ index.js
@@ -21,6 +21,65 @@ slackTerminal.init(token, {
 });
 
 ///////////////// Self Updater /////////////////
+var AutoUpdater = require('auto-updater');
+
+var autoupdater = new AutoUpdater({
+  pathToJson: '/',
+  autoupdate: true,
+  checkgit: false,
+  jsonhost: 'raw.githubusercontent.com',
+  contenthost: 'codeload.github.com',
+  progressDebounce: 0,
+  devmode: false
+});
+
+// State the events
+autoupdater.on('git-clone', function () {
+  console.log("You have a clone of the repository. Use 'git pull' to be up-to-date");
+});
+autoupdater.on('check.up-to-date', function (v) {
+  console.info("You have the latest version: " + v);
+});
+autoupdater.on('check.out-dated', function (v_old, v) {
+  console.warn("Your version is outdated. " + v_old + " of " + v);
+  autoupdater.fire('download-update'); // If autoupdate: false, you'll have to do this manually.
+  // Maybe ask if the'd like to download the update.
+});
+autoupdater.on('update.downloaded', function () {
+  console.log("Update downloaded and ready for install");
+  autoupdater.fire('extract'); // If autoupdate: false, you'll have to do this manually.
+});
+autoupdater.on('update.not-installed', function () {
+  console.log("The Update was already in your folder! It's read for install");
+  autoupdater.fire('extract'); // If autoupdate: false, you'll have to do this manually.
+});
+autoupdater.on('update.extracted', function () {
+  console.log("Update extracted successfully!");
+  console.warn("RESTART THE APP!");
+});
+autoupdater.on('download.start', function (name) {
+  console.log("Starting downloading: " + name);
+});
+autoupdater.on('download.progress', function (name, perc) {
+  process.stdout.write("Downloading " + perc + "% \033[0G");
+});
+autoupdater.on('download.end', function (name) {
+  console.log("Downloaded " + name);
+});
+autoupdater.on('download.error', function (err) {
+  console.error("Error when downloading: " + err);
+});
+autoupdater.on('end', function () {
+  console.log("The app is ready to function");
+});
+autoupdater.on('error', function (name, e) {
+  console.error(name, e);
+});
+
+// Start checking
+autoupdater.fire('check');
+//autoupdater.fire('download-update');
+//autoupdater.fire('extract');
 
 ///////////////// GitHiub Listener Server /////////////////
 
@@ -32,9 +91,9 @@ var postRequest = require('./commands/postRequest.js');
 
 http.createServer(function (req, res) {
   handler(req, res, function (err) {
-    res.statusCode = 404;
-    res.end('no such location');
-  });
+    res.statusCode = 404
+    res.end('no such location')
+  })
 }).listen(4567);
 
 handler.on('error', function (err) {
@@ -51,12 +110,12 @@ handler.on('push', function (event) {
   console.log('Received a push event for %s to %s',
           event.payload.repository.name,
           event.payload.ref);
-//autoupdater.fire('check');
+autoupdater.fire('check');
 
 
 });
 
 // and Finally
-console.log("Test Commit IV");
+console.log("Test Commit IVXX");
 
 
